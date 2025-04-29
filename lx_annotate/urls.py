d9070a7d990c57ce7abae0bbce5d6959ca519cf5
maxhild <maxhild10@gmail.com>
2025-04-29 10:31:05 +0200

    Adding settings for the url configuration

--- lx_annotate/urls.py
+++ lx_annotate/urls.py
@@ -9,7 +9,10 @@ from lx_annotate.views import ProxyView
 urlpatterns = [
     # API endpoints for authentication etc.
     path('admin/', admin.site.urls),
-    path('api/<path:endpoint>', ProxyView.as_view()),  # Proxy API requests
+    #path('api/<path:endpoint>', ProxyView.as_view()),  # Proxy API requests
     # Catch-all for Vue.js SPA (only for non-API and non-admin routes)
+
+    # Mount all endoreg-db endpoints under "/endo-reg/"
+    path('/', include('endoreg_db.urls')),
     re_path(r'^(?!api/|admin/).*$', TemplateView.as_view(template_name='base.html'), name='vue_spa'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
