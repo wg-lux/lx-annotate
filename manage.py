48215b2f0c2eb5f0dad9783ecdfebbb94a1fd404
maxhild <maxhild10@gmail.com>
2025-06-09 16:05:09 +0200

    different settings module

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'agl_anonymizer.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lx-annotate.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
