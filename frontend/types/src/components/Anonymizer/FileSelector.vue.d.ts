68cd7372c9199926da53b393368210a4b5349ed3
maxhild <maxhild10@gmail.com>
2025-07-02 18:17:54 +0200

    new main - direnv setup improved for hopefully better loading time

--- frontend/types/src/components/Anonymizer/FileSelector.vue.d.ts
+++ frontend/types/src/components/Anonymizer/FileSelector.vue.d.ts
@@ -1,28 +1,2 @@
-interface PatientInfo {
-    patientFirstName?: string;
-    patientLastName?: string;
-    patientDob?: string;
-    examinationDate?: string;
-    centerName?: string;
-}
-interface FileItem {
-    id: number;
-    filename: string;
-    filePath?: string;
-    sensitiveMetaId?: number;
-    patientInfo?: PatientInfo;
-    createdAt?: string;
-}
-interface SelectedFile {
-    type: 'pdf' | 'video';
-    id: number;
-    data: FileItem;
-}
-declare const _default: import("vue").DefineComponent<{}, {}, {}, {}, {}, import("vue").ComponentOptionsMixin, import("vue").ComponentOptionsMixin, {
-    fileSelected: (file: SelectedFile) => any;
-    cancel: () => any;
-}, string, import("vue").PublicProps, Readonly<{}> & Readonly<{
-    onCancel?: (() => any) | undefined;
-    onFileSelected?: ((file: SelectedFile) => any) | undefined;
-}>, {}, {}, {}, {}, string, import("vue").ComponentProvideOptions, true, {}, HTMLDivElement>;
+declare const _default: import("vue").DefineComponent<{}, {}, {}, {}, {}, import("vue").ComponentOptionsMixin, import("vue").ComponentOptionsMixin, {}, string, import("vue").PublicProps, Readonly<{}> & Readonly<{}>, {}, {}, {}, {}, string, import("vue").ComponentProvideOptions, true, {}, any>;
 export default _default;
