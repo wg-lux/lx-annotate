820a0ab841664ec611f28b24a312ca45f1fb4c86
maxhild <maxhild10@gmail.com>
2025-11-06 12:54:11 +0100

    Ensuring correct env setup, fixed daphne conf and added daphne for systemmd service

--- devenv/management.nix
+++ devenv/management.nix
@@ -134,10 +134,14 @@ in
     
 
     "run-server".exec = ''
-      export DJANGO_HOST="''${DJANGO_HOST:-${appConfig.server.host}}"
-      export DJANGO_PORT="''${DJANGO_PORT:-${appConfig.server.port}}"
-      bash scripts/core/server-run.sh
+    export DJANGO_SETTINGS_MODULE="''${DJANGO_SETTINGS_MODULE:-lx_annotate.settings_prod}"
+    export DJANGO_HOST="''${DJANGO_HOST:-${appConfig.server.host}}"
+    export DJANGO_PORT="''${DJANGO_PORT:-${appConfig.server.port}}"
+
+    echo "🌀 Starting Daphne on ${appConfig.server.host}:${appConfig.server.port}..."
+    exec daphne -b "''${DJANGO_HOST}" -p "''${DJANGO_PORT}" lx_annotate.asgi:application
     '';
+
 
     "docker-dev-build".exec = ''
       set -e
