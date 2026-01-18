072cfdc80ef29c5a513e0238adef0778fb605fa5
maxhild <maxhild10@gmail.com>
2026-01-18 15:49:02 +0100

    cors origins

--- lx_annotate/settings/settings_base.py
+++ lx_annotate/settings/settings_base.py
@@ -9,8 +9,6 @@ from pathlib import Path
 from lx_annotate.settings.config import load_config
 from logging import getLogger
 
-from lx_annotate.settings.settings_prod import CORS_ALLOWED_ORIGINS
-
 logger = getLogger(__name__)
 # -----------------------------------------------------------------------------
 # 1. PATH CONFIGURATION
