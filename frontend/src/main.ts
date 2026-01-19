ee4fd905bd09384e412d68347d68685fc8b50eed
Thomas <hamza.ukw@gmail.com>
2026-01-19 17:53:12 +0100

    login logout

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -3,11 +3,14 @@ import { createPinia } from 'pinia'
 import App from '@/App.vue'
 import router from '@/router'
 
+
 import '@/assets/css/nucleo-icons.css'
 import '@/assets/css/nucleo-svg.css'
 import '@/assets/css/material-dashboard.css'
 import '@/assets/css/icon-fixes.css'
 import '@/assets/custom-overrides.css'
+
+import 'vite/modulepreload-polyfill'
 
 import VueVirtualScroller from 'vue-virtual-scroller'
 import 'vue-virtual-scroller/dist/vue-virtual-scroller.css'
