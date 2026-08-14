1c27329cb7e9dabd2a2d36490f3e29bbb225d874
maxhild <maxhild10@gmail.com>
2026-08-14 14:28:49 +0200

    hub transfer hashing

--- frontend/default.nix
+++ frontend/default.nix
@@ -14,7 +14,7 @@ buildNpmPackage rec {
 # specified: the SHA-AA... default
 # (fake) got (your real hash)
 # Replace lib.fakeHash with the returned hash in default.nix
-  npmDepsHash = "sha256-NFZnnW5V9b2yxUQRE8inABsw/oJdMns9kr45AUgzsWw=";
+  npmDepsHash = "sha256-ZsegTaP8vfbfi8q4aJH0AUmU1E91cOljrYQNVC8Del4=";
 
   npmBuildScript = "build";
   doCheck = false;
